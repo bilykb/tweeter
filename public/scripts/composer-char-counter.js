1ba36e56d9ecd3d12b9b2fb127b3efd4ed67be5f
bilykb <brettbilyk@gmail.com>
2021-09-10 23:40:55 +0000

    Add jQuery event listener for #tweet-text. Updates the character count in the UI and changes the CSS color depending on the number of characters remaining

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function() {
+  console.log("DOM is loaded");
+
+  $("#tweet-text").on("input", function() {
+    const numOfCharsInText = $(this).val().length;
+    const remainingChars = `${140 - numOfCharsInText}`;
+    const counterUI = $(this).closest("section").find("output").html(remainingChars);
+
+    if (remainingChars <= 10 && remainingChars > 0) {
+      $(counterUI).css("color", "#E5CB20");
+    } else if (remainingChars <= 0) {
+      $(counterUI).css("color", "#C70000")
+    } else {
+      $(counterUI).css("color", "#545149")
+    }
+  });
+});
