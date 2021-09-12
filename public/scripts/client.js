164653c24c31a78b2ee6307d861b92c11f46a524
bilykb <brettbilyk@gmail.com>
2021-09-12 21:47:25 +0000

    Add .css for posted-tweet.css. Name change from old-tweet.css to posted-tweet.css Additional small changes made to new-tweet.css (hover added to submit button). Some utility classes added to layout.css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+   // fetching data from JSON file
+   $.get("initial-tweets.json", function(data) {
+
+   });
+});
