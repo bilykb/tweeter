eb6acccd1990231c929e2ace50bd5d7c14ac29cf
bilykb <brettbilyk@gmail.com>
2021-09-13 21:20:45 +0000

    Remove incorrect code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,3 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-   // fetching data from JSON file
-   $.get("initial-tweets.json", function(data) {
-
-   });
-});
