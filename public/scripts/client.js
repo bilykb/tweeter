4b6f5fc3ab470f1777b0dfdc20e395626e4505ec
bilykb <brettbilyk@gmail.com>
2021-09-14 00:31:58 +0000

    Add function to render all tweets in object database renderTweets() utilizes createTweetElements() to render all the necessary HTML code for each tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-   "user": {
-     "name": "Newton",
-     "avatars": "https://i.imgur.com/73hZDYK.png",
+$(document).ready(function() {
+
+const data = [
+   {
+     "user": {
+       "name": "Newton",
+       "avatars": "https://i.imgur.com/73hZDYK.png"
+       ,
        "handle": "@SirIsaac"
      },
-   "content": {
+     "content": {
        "text": "If I have seen further it is by standing on the shoulders of giants"
      },
-   "created_at": 1461116232227
-}
+     "created_at": 1461116232227
+   },
+   {
+     "user": {
+       "name": "Descartes",
+       "avatars": "https://i.imgur.com/nlhLi3I.png",
+       "handle": "@rd" },
+     "content": {
+       "text": "Je pense , donc je suis"
+     },
+     "created_at": 1461113959088
+   }
+ ]
 
 const createTweetElement = function(tweetData) {
    return `<article class="tweet">
@@ -39,10 +54,15 @@ const createTweetElement = function(tweetData) {
  </article>`
 }
 
+const renderTweets = function(tweets) {
 
-const $tweet = createTweetElement(tweetData);
+  for (let [postedTweet] in tweets) {
+    let $tweet = createTweetElement(tweets[postedTweet])
 
-$(document).ready(function() {
-   console.log($tweet);
-   $('#tweet-container').append($tweet);
+      $('#tweet-container').append($tweet);
+  }
+};
+
+renderTweets(data);
+  
 });
