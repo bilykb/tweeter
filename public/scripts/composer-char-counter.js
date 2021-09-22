f80044118176c8b32841b06b5c3bbc8dc87b3087
bilykb <brettbilyk@gmail.com>
2021-09-22 23:08:50 +0000

    Add $ to variables in order to indicate the use of jQuery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,13 +2,13 @@
  * jQuery function that keeps track of the number of characters in .tweet-text, changes the color of counter text depending on num of remaining chars
  */
 
-$(document).ready(function() {
+$(document).ready(function characterCounter() {
   console.log("DOM is loaded");
 
   $(".tweet-text").on("input", function() {
     const $numOfCharsInText = $(this).val().length;
     const $remainingChars = `${140 - $numOfCharsInText}`;
-    const $counterUI = $(this).closest("section").find("output").html(remainingChars);
+    const $counterUI = $(this).closest("section").find("output").html($remainingChars);
 
     if ($remainingChars <= 10 && $remainingChars > 0) {
       $($counterUI).css("color", "#E5CB20");
