93825eadce99593ad688185a2aec7f558334399a
bilykb <brettbilyk@gmail.com>
2021-09-23 01:43:29 +0000

    Export scrollToTop()

--- public/scripts/scrollToTop.js
+++ public/scripts/scrollToTop.js
@@ -2,7 +2,7 @@
  * Function used with .scrollToTop button which allows user to scroll to top of page
  */
 
-const scrollToTop = function() {
+export const scrollToTop = function() {
   window.scrollTo({
     top: 0,
     left: 0,
